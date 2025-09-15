5210d5402acfe181272f74b283761e86162b6e2c
Arnaud Ferrand <arferrand@gmail.com>
2025-09-15 09:43:36 +0100

    test default import

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { geocode, GeocodeRequest } from 'opencage-api-client';
+import opencage, { GeocodingRequest } from 'opencage-api-client';
 
 export const doGeocode = async () => {
-  const input: GeocodeRequest = {
+  const input: GeocodingRequest = {
     q: '51.952659,7.632473',
     key: '6d0e711d72d74daeb2b0bfd2a5cdfdba', // https://opencagedata.com/api#testingkeys
     no_annotations: 1,
   };
-  const result = await geocode(input);
-  console.log(JSON.stringify(result,null,2));
+  const result = await opencage.geocode(input);
+  console.log(JSON.stringify(result, null, 2));
   return result;
 };
